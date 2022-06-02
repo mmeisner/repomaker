8544ba67569cea2fbcd165176ddf2a36a73c2d1a
Mads Meisner-Jensen <mmeisner@users.noreply.github.com>
2022-06-02 21:00:31 +0200

    log: refactor and ensure Log.style is empty when Ansi colors off

--- repomaker/log.py
+++ repomaker/log.py
@@ -25,19 +25,6 @@ class Ansi:
         pink0,   pink1,   pink2,   pink3   = "", "", "", ""
         purple0, purple1, purple2, purple3 = "", "", "", ""
 
-    class style:
-        note = "fg.iyellow"
-        info = "fg.white bold"
-        norm = ""
-        verb = "fg.white"
-        debug = "fg.icyan"
-        trace = "fg.cyan italic"
-        warn = "fg.iyellow"
-        error = "fg.ired"
-        tip = "fg.yellow"
-        shell = "fg.igreen"
-        lite = "fg.white bold"
-
     _initialized = False
 
     @staticmethod
@@ -51,10 +38,12 @@ class Ansi:
         """
         Ansi._initialized = True
 
+        colorize = force or (sys.stdout.isatty() and os.name != 'nt')
+        if not colorize:
+            return
+
         # dicts are ordered by default from Python 3.6
         # and thus this code will only work with Python 3.6+
-        if not (force or (sys.stdout.isatty() and os.name != 'nt')):
-            return
 
         # codes for foreground
         # black, red, green, yellow, blue, magenta, cyan, white
@@ -87,20 +76,6 @@ class Ansi:
                 code = next(codes)
                 setattr(Ansi.fg8, k, f"\033[38:5:{code}m")
 
-        # expand the colors into the Ansi.style attributes
-        for k,v in Ansi.style.__dict__.items():
-            if type(Ansi.style.__dict__[k]) == str and not k.startswith("__"):
-                names = v.split(" ")
-                s = ""
-                for name in names:
-                    if not name:
-                        continue
-                    if name.startswith("fg."):
-                        s += Ansi.fg.__dict__[name[3:]]
-                    else:
-                        s += Ansi.__dict__[name]
-                setattr(Ansi.style, k, s)
-
 
 class Log(object):
     """
@@ -115,6 +90,21 @@ class Log(object):
         Trace = 3
         Crazy = 4
 
+    class style:
+        note = "fg.iyellow"
+        info = "fg.white bold"
+        norm = ""
+        verb = "fg.white"
+        debug = "fg.icyan"
+        trace = "fg.cyan italic"
+        warn = "fg.iyellow"
+        error = "fg.ired"
+        tip = "fg.yellow"
+        shell = "fg.igreen"
+        lite = "fg.white bold"
+
+    _initialized = False
+
     def __init__(self, level=0, with_progress=False, with_tips=True, with_shell=True):
         self.level = level
         self.with_progress = with_progress
@@ -123,54 +113,64 @@ class Log(object):
         # True to show shell commands
         self.enable_shell = with_shell
 
+        if Log._initialized:
+            return
+
         # initialize ANSI colors if user forgot to do so
         if not Ansi._initialized:
             Ansi.enable()
 
+        Log._initialized = True
+        # expand the colors into the Log.style attributes
+        for k,v in Log.style.__dict__.items():
+            if type(Log.style.__dict__[k]) == str and not k.startswith("__"):
+                names = v.split(" ")
+                s = ""
+                for name in names:
+                    if not name:
+                        continue
+                    if name.startswith("fg."):
+                        s += Ansi.fg.__dict__[name[3:]]
+                    else:
+                        s += Ansi.__dict__[name]
+                setattr(Log.style, k, s)
+
     def note(self, s, level=-1):
         """Log notice message that is more noteworthy than `info`"""
         if self.level >= level:
-            print(f"{Ansi.style.note}{s}{Ansi.reset}")
+            print(f"{Log.style.note}{s}{Ansi.reset}")
 
     def info(self, s, level=0):
         """Log normal info message colorized specially"""
         if self.level >= level:
-            print(f"{Ansi.style.info}{s}{Ansi.reset}")
+            print(f"{Log.style.info}{s}{Ansi.reset}")
 
     def norm(self, s, level=0):
         """Log normal info message (usaully neutral/white color)"""
         if self.level >= level:
-            print(f"{Ansi.style.norm}{s}{Ansi.reset}")
+            print(f"{Log.style.norm}{s}{Ansi.reset}")
 
     def verb(self, s, level=1):
         """Log message at verbose level (more detail)"""
         if self.level >= level:
-            print(f"{Ansi.style.verb}{s}{Ansi.reset}")
+            print(f"{Log.style.verb}{s}{Ansi.reset}")
 
     def debug(self, s, level=2):
         """Log message at debug level (lots of detail)"""
         if self.level >= level:
-            print(f"{Ansi.style.debug}{s}{Ansi.reset}")
+            print(f"{Log.style.debug}{s}{Ansi.reset}")
 
     def trace(self, s, level=3):
         """Log message at trace level (tons of detail)"""
         if self.level >= level:
-            print(f"{Ansi.style.trace}{s}{Ansi.reset}")
+            print(f"{Log.style.trace}{s}{Ansi.reset}")
 
     def error(self, s):
-        print(f"{Ansi.style.error}ERROR: {s}{Ansi.reset}", file=sys.stderr)
+        print(f"{Log.style.error}ERROR: {s}{Ansi.reset}", file=sys.stderr)
 
     def warn(self, s):
-        print(f"{Ansi.style.warn}WARNING: {s}{Ansi.reset}", file=sys.stderr)
-
-    def lite(self, s, level=0):
-        if self.level >= level:
-            print(f"{Ansi.style.lite}{s}{Ansi.reset}")
-
-    def tip(self, s):
-        if self.enable_tips:
-            print(f"{Ansi.style.tip}Tip: {s}{Ansi.reset}")
+        print(f"{Log.style.warn}WARNING: {s}{Ansi.reset}", file=sys.stderr)
 
     def shell(self, s):
         if self.enable_shell:
-            print(f"{Ansi.style.shell}{s}{Ansi.reset}")
+            print(f"{Log.style.shell}{s}{Ansi.reset}")
